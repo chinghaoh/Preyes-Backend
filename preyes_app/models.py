4393d683a896fba3cc7456db975bb7b5d774836d
Didier Guyon <didier_pietro@hotmail.com>
2021-02-17 15:51:20 +0100

    feat(models): Added user, customer and admin model

--- preyes_app/models.py
+++ preyes_app/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
-models
+class User(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    insertion = models.CharField(max_length=30, null=True, blank=True)
+    email = models.CharField(max_length=100, null=False)
+    notifications = models.BooleanField(default=False)
+    birth_date = models.DateField(null=False)
+
+    class Meta:
+        abstract = True
+
+
+class Admin(User):
+
+    def get_analytics(self):
+        pass
+
+
+class Customer(User):
+    pass
