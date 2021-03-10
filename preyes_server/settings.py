0820ab70fd99d753d9f7c62eb4c598194f1095ac
Didier Guyon <didier_pietro@hotmail.com>
2021-03-10 18:17:15 +0100

    fix(settings.py): Removed security settings

--- preyes_server/settings.py
+++ preyes_server/settings.py
@@ -62,10 +62,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'preyes_server.urls'
 
 # Security settings
-
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # These setting prevents browsers from connecting with our site via http
 # TODO: Comment these settings below when developing locally
