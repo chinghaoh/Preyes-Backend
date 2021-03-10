efa89b299316d9bcfe0414ce62bbf56eaed698ad
Didier Guyon <didier_pietro@hotmail.com>
2021-03-10 18:58:20 +0100

    fix(settings.py): Routing issue

--- preyes_server/settings.py
+++ preyes_server/settings.py
@@ -29,9 +29,9 @@ env.read_env(env_file)
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['preyesserver.herokuapp.com']
+ALLOWED_HOSTS = ['preyesserver.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -150,12 +150,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
