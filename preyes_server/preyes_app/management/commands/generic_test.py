f8608da2838f75584d015f638f5df5a671dd8fee
Didier Guyon <didier_pietro@hotmail.com>
2021-03-11 13:21:19 +0100

    fix(generic_test): reverted generic_test file

--- preyes_server/preyes_app/management/commands/generic_test.py
+++ preyes_server/preyes_app/management/commands/generic_test.py
@@ -1,10 +1,8 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-from preyes_server.preyes_app.models import Customer
 
 
 class Command(BaseCommand):
     help = 'Try out test code snippets here'
 
     def handle(self, *args, **kwargs):
-        customer = Customer.objects.get(email="Testing@tester.com")
+        pass
