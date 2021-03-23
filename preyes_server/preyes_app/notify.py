64c14e7c11a36610050cb8c33a4201c943185c80
Didier Guyon <didier_pietro@hotmail.com>
2021-03-23 16:00:28 +0100

    fix(notify): added client_id to params

--- preyes_server/preyes_app/notify.py
+++ preyes_server/preyes_app/notify.py
@@ -2,13 +2,13 @@ from fcm_django.models import FCMDevice
 import environ
 
 
-def notify(title, body, data, sound=False):
+def notify(client_id, title, body, data, sound=False):
     root_dir = environ.Path(__file__) - 4
     env = environ.Env()
     env_file = str(root_dir.path('.env'))
     env.read_env(env_file)
     try:
-        device = FCMDevice.objects.first()
+        device = FCMDevice.objects.get(user=client_id)
         result = device.send_message(title=title, body=body, data=data, sound=sound,
                                      api_key=env('FCM_KEY'))
         return result['success'] if result['success'] == 1 else result['failure']
