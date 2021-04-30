c6b0ec7f46b295b3e2f65dba4d0ad22faa52bd1f
Didier Guyon <didier_pietro@hotmail.com>
2021-04-30 17:34:44 +0200

    fix(Cronjobs): revert

--- preyes_server/settings.py
+++ preyes_server/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import environ
 import os
 import dj_database_url
-import django
-django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
